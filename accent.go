4846c2da736a926548d1d646bafef3f1202d6c7f
Chuck Preslar <chuckpreslar@gmail.com>
2013-10-08 11:43:16 -0400

    Accent package documentation.

--- accent.go
+++ accent.go
@@ -1,3 +1,5 @@
+// Package accent provides color and decoration accents
+// for terminal output.
 package accent
 
 import "fmt"
@@ -26,9 +28,13 @@ const (
 )
 
 type accentuator interface {
+	// accentuate accepts and interface and returns
+	// an accented string.
 	accentuate(interface{}) string
 }
 
+// colors in a map of color types to the format
+// string that provides the accent.
 var colors = map[color]string{
 	White:   "\x1B[37m%v\x1B[39m",
 	Grey:    "\x1B[90m%v\x1B[39m",
@@ -41,6 +47,8 @@ var colors = map[color]string{
 	Yellow:  "\x1B[33m%v\x1B[39m",
 }
 
+// decorations in a map of decoration types to the
+// format string that provides the accent.
 var decorations = map[decoration]string{
 	Bold:          "\x1B[1m%v\x1B[22m",
 	Italic:        "\x1B[3m%v\x1B[23m'",
@@ -48,16 +56,22 @@ var decorations = map[decoration]string{
 	Strikethrough: "\x1B[9m%v\x1B[29m",
 }
 
+// accentuate implements the accentuator interface
+// for the color type.
 func (c color) accentuate(input interface{}) string {
 	return fmt.Sprintf(colors[c], input)
 }
 
+// accentuate implements the accentuator interface
+// for the decoration type.
 func (d decoration) accentuate(input interface{}) string {
 	return fmt.Sprintf(decorations[d], input)
 }
 
+// Message formats a string based on the input parameter
+// accented with the provided accentuators return the
+// resulting string.
 func Message(input interface{}, accents ...accentuator) string {
-
 	for i := 0; i < len(accents); i++ {
 		input = accents[i].accentuate(input)
 	}
